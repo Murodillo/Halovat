d3e83806b80c6cdae485c7d4aa5b169c0381c277
Murodillo Ismatov <ismatovmurodillo@gmail.com>
2021-05-22 11:32:49 +0500

    - Map is setted with markers - Firebase and google maps connected

--- app/src/main/java/uz/halovat/halovat/Menus/MosquesMapActivity.kt
+++ app/src/main/java/uz/halovat/halovat/Menus/MosquesMapActivity.kt
@@ -2,11 +2,44 @@ package uz.halovat.halovat.Menus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.menu_toolbar.*
 import uz.halovat.halovat.R
 
-class MosquesMapActivity : AppCompatActivity() {
+class MosquesMapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mosques_map)
+        setSupportActionBar(toolbarformenu)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Xarita"
+        toolbarformenu.setNavigationOnClickListener {
+            onBackPressed()
+        }
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val innovation = LatLng(41.348710, 69.208313)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(innovation)
+                .title("Minestry of Innovation")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(innovation, 15f))
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.anim_left_to_right_exit, R.anim.anim_right_to_left_exit)
+
     }
 }
