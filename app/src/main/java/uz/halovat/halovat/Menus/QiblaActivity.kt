9cc1bb25e43e41f125c5260d23f139f261286d30
Murodillo Ismatov <ismatovmurodillo@gmail.com>
2021-05-22 11:41:55 +0500

    - Qibla page UI is ready - Sensor working but it is not clear I think

--- app/src/main/java/uz/halovat/halovat/Menus/QiblaActivity.kt
+++ app/src/main/java/uz/halovat/halovat/Menus/QiblaActivity.kt
@@ -1,12 +1,83 @@
 package uz.halovat.halovat.Menus
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.location.Location
+import android.location.LocationListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import kotlinx.android.synthetic.main.activity_qibla.*
+import kotlinx.android.synthetic.main.menu_toolbar.*
 import uz.halovat.halovat.R
+import kotlin.math.roundToInt
 
-class QiblaActivity : AppCompatActivity() {
+class QiblaActivity : AppCompatActivity(), SensorEventListener, LocationListener {
+    private lateinit var sensorManager: SensorManager
+    private var mLight: Sensor? = null
+    var current: Float = 242.0f
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qibla)
+        setSupportActionBar(toolbarformenu)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+        toolbarformenu.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        mLight = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION)
+
+
+    }
+
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.anim_left_to_right_exit, R.anim.anim_right_to_left_exit)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mLight?.also { light ->
+            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_GAME)
+
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+
+        val degree: Float = event.values[0].roundToInt().toFloat()
+        val animation: RotateAnimation = RotateAnimation(
+            current, degree, Animation.RELATIVE_TO_SELF, 0.5f,
+            Animation.RELATIVE_TO_SELF, 0.5f
+        )
+        animation.duration = 100
+        animation.fillAfter = true
+        iv_compass.startAnimation(animation)
+        current = degree
+
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
+    }
+
+    override fun onLocationChanged(location: Location) {
+
     }
 }
---
+++ app/src/main/java/uz/halovat/halovat/Model/LocationModel.kt
@@ -0,0 +1,7 @@
+package uz.halovat.halovat.Model
+
+data class LocationModel(
+    val address: String?,
+    val lat: Double,
+    val lng: Double
+)
