15d117b1d7f09d039e2dee27bf80786a80ee7996
Murodillo Ismatov <ismatovmurodillo@gmail.com>
2021-05-22 13:32:50 +0500

    - Question activity ready

--- app/src/main/java/uz/halovat/halovat/Menus/MosqueInfoActivity.kt
+++ app/src/main/java/uz/halovat/halovat/Menus/MosqueInfoActivity.kt
@@ -1,7 +1,9 @@
 package uz.halovat.halovat.Menus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_mosque_info.*
 import kotlinx.android.synthetic.main.menu_toolbar.*
 import uz.halovat.halovat.R
 
@@ -15,6 +17,14 @@ class MosqueInfoActivity : AppCompatActivity() {
         toolbarformenu.setNavigationOnClickListener {
             onBackPressed()
         }
+
+
+        cv_aloqa.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(R.anim.anim_right_to_left, R.anim.anim_left_to_right)
+        }
+
     }
 
     override fun onBackPressed() {
--- app/src/main/java/uz/halovat/halovat/Menus/QuestionActivity.kt
+++ app/src/main/java/uz/halovat/halovat/Menus/QuestionActivity.kt
@@ -2,11 +2,32 @@ package uz.halovat.halovat.Menus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_question.*
+import kotlinx.android.synthetic.main.menu_toolbar.*
 import uz.halovat.halovat.R
 
 class QuestionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
+        setSupportActionBar(toolbarformenu)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = ""
+        toolbarformenu.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+
+        cv_send.setOnClickListener {
+            Toast.makeText(this, "Savol yo'llandi...", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.anim_left_to_right_exit, R.anim.anim_right_to_left_exit)
+
     }
 }
